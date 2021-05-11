4b20bd7eae92a91274a243a2466413fd806e5b6d
jorge.melgoza <jorge.melgoza@warnermedia.com>
2021-05-11 16:07:28 -0500

    place bid changes

---
+++ src/handlers/placeBid.js
@@ -0,0 +1,44 @@
+import AWS from 'aws-sdk';
+import commonMiddleware from '../util/commonMiddleware'
+import createError from 'http-errors'
+import { getAuctionById } from './getAuction'
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function placeBid(event, context) {
+    const { id } = event.pathParameters;
+    const { amount } = event.body;
+
+    const auction = await getAuctionById(id);
+
+    if(amount <= auction.highestBid.amount){
+        throw new createError.BadRequest(`Bid must be higher than the last recorded: ${auction.highestBid.amount}`);
+    }
+
+    const params = {
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Key: { id },
+        UpdateExpression: 'set highestBid.amount = :amount',
+        ExpressionAttributeValues: {
+            ':amount': amount,
+        },
+        ReturnValues: 'ALL_NEW',
+    }
+
+    let updatedAuction;
+
+    try {
+        const  result = await dynamodb.update(params).promise();
+        updatedAuction = result.Attributes;
+    } catch (error) {
+        console.log(error);
+        throw new createError.InternalServerError(error);
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(updatedAuction),
+    };
+}
+
+export const handler = commonMiddleware(placeBid);
