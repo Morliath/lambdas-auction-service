44cadc1abddc2c83fd65d1904a11783df3784437
jorge.melgoza <jorge.melgoza@warnermedia.com>
2021-05-13 14:46:58 -0500

    Close auctions logic

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,4 +1,17 @@
+import { getFinishedAuctions,  closeAuction}  from '../service/auctions'
+import createError from 'http-errors'
+
 async function processAuctions(event, context) {
+    try {
+        const auctionsToClose = await getFinishedAuctions();
+        const closePromises = auctionsToClose.map( auction => closeAuction(auction));
+        await Promise.all(closePromises);
+
+        return { closed: closePromises.length};
+    } catch (error) {
+        console.log(error);
+        throw new createError.InternalServerError(error);
+    }
 
 }
 
