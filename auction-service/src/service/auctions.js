398ff16816e7947befd2ccddb05ebbf8ff57410f
jorge.melgoza <jorge.melgoza@warnermedia.com>
2021-05-19 10:52:33 -0500

    Close auction notifications

--- auction-service/src/service/auctions.js
+++ auction-service/src/service/auctions.js
@@ -3,7 +3,7 @@ import AWS from 'aws-sdk';
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 export async function getFinishedAuctions() {
-    const now  = new Date();
+    const now = new Date();
 
     const params = {
         TableName: process.env.AUCTIONS_TABLE_NAME,
@@ -24,6 +24,7 @@ export async function getFinishedAuctions() {
 }
 
 export async function closeAuction(auction) {
+    const sqs = new AWS.SQS();
     const params = {
         TableName: process.env.AUCTIONS_TABLE_NAME,
         Key: { id: auction.id },
@@ -36,5 +37,31 @@ export async function closeAuction(auction) {
         }
     }
 
-    return await dynamodb.update(params).promise();
+    const result = await dynamodb.update(params).promise();
+
+    const { title, seller, highestBid } = auction;
+    const { amount, bidder } = highestBid;
+
+    if(bidder){
+        const notifySeller = buildMessage('Your item has been sold', seller, `Your item ${title} has been sold for ${amount}`);
+        const notifyBidder = buildMessage('You won an auction', bidder, `You won item ${title} with highest bid of ${amount}`);
+        Promise.all([notifySeller, notifyBidder]);
+
+        return result;
+    } else {
+        await buildMessage('No bids on your auction', seller, `Your item ${title} has no bids and got expired`);
+        return;
+    }
+
+}
+
+function buildMessage(subject, recipient, body) {
+    return sqs.sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+            subject,
+            recipient,
+            body,
+        })
+    }).promise();
 }
